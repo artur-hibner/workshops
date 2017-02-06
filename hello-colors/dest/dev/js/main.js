0d16512e97726731579e638e873f8cb0adbf960f
artur-hibner <artur@hiweb.pl>
2017-02-06 16:18:35 +0100

    burger-menu

---
+++ hello-colors/dest/dev/js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	$("#burger-container").on('click', function(){
+		$(this).toggleClass("open");
+		$("#main-menu").toggleClass("open");
+	});
+});
+//# sourceMappingURL=main.js.map
