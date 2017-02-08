2424d4a6bbb30f842159a56d8d8b8ad852fe96f4
artur-hibner <artur@hiweb.pl>
2017-02-08 16:26:22 +0100

    back to top button

--- hello-colors/dest/dev/js/main.js
+++ hello-colors/dest/dev/js/main.js
@@ -3,5 +3,23 @@ $(document).ready(function(){
 		$(this).toggleClass("open");
 		$("#main-menu").toggleClass("open");
 	});
+
+	$(window).scroll(function() {
+	    if ($(this).scrollTop() > 200) {   
+	    	$('#return-to-top').fadeIn(200); 
+
+	    } else {
+	    	$('#return-to-top').fadeOut(200);
+	    }
+	});
+
+	$('#return-to-top').click(function() {      
+	    $('body,html').animate({
+	        scrollTop : 0                       
+	    },1000);
+	});
+
 });
+
+
 //# sourceMappingURL=main.js.map
--- hello-colors/src/js/main.js
+++ hello-colors/src/js/main.js
@@ -3,4 +3,21 @@ $(document).ready(function(){
 		$(this).toggleClass("open");
 		$("#main-menu").toggleClass("open");
 	});
+
+	$(window).scroll(function() {
+	    if ($(this).scrollTop() > 200) {   
+	    	$('#return-to-top').fadeIn(200); 
+
+	    } else {
+	    	$('#return-to-top').fadeOut(200);
+	    }
+	});
+
+	$('#return-to-top').click(function() {      
+	    $('body,html').animate({
+	        scrollTop : 0                       
+	    },1000);
+	});
+
 });
+
